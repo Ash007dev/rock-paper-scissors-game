9dab4ffb7745d6a9c135c7fcfb680c57ff41f459
Ash <ashish007tup@gmail.com>
2025-08-12 17:48:03 +0530

    Auto Play feature added

--- script.js
+++ script.js
@@ -18,52 +18,84 @@ if (scores === null){
     };
         
 */
-function computeRPS(userChoice) {
+
+let isAutoPlay = false;
+let intervalId;
+
+function autoPlay(){
+
+    if(!isAutoPlay){
+        isAutoPlay = true;
+        intervalId = setInterval(function() {
+            const playerMove = compMove();
+            computeRPS(playerMove);
+        },1000)
+    }
+    else{
+        clearInterval(intervalId);
+        isAutoPlay = false;
+    }
+}
+
+
+function compMove(){
+
     const randChoice = Math.random();
-    let userMove = userChoice;
-    let compMove = '';
-    let result = '';
-    let out = '';
+    let computerMove = '';
     if (randChoice >= 0 && randChoice <= 1 / 3) {
-        compMove = 'rock';
+        computerMove = 'rock';
     }
 
     else if (randChoice > 1 / 3 && randChoice <= 2 / 3) {
-        compMove = 'paper';
+        computerMove = 'paper';
     }
     else if (randChoice > 2 / 3 && randChoice <= 1) {
-        compMove = 'scissors';
+        computerMove = 'scissors';
     }
 
+    return computerMove;
 
-    if ((compMove === 'rock' && userMove === 'rock') || (compMove === 'paper' && userMove === 'paper') || (compMove === 'scissors' && userMove === 'scissors')) {
+}
+
+function computeRPS(userChoice) {
+
+    let userMove = userChoice;
+    let computerMove = compMove();
+    let result = '';
+
+
+    if ((computerMove === 'rock' && userMove === 'rock') || (computerMove === 'paper' && userMove === 'paper') || (computerMove === 'scissors' && userMove === 'scissors')) {
         result = 'TIE';
         scores.ties += 1;
     }
 
-    else if ((compMove === 'paper' && userMove === 'rock') || (compMove === 'rock' && userMove === 'scissors') || (compMove === 'scissors' && userMove === 'paper')) {
+    else if ((computerMove === 'paper' && userMove === 'rock') || (computerMove === 'rock' && userMove === 'scissors') || (computerMove === 'scissors' && userMove === 'paper')) {
         result = 'YOU LOSE';
         scores.losses += 1;
     }
 
-    else if ((compMove === 'rock' && userMove === 'paper') || (compMove === 'paper' && userMove === 'scissors') || (compMove === 'scissors' && userMove === 'rock')) {
+    else if ((computerMove === 'rock' && userMove === 'paper') || (computerMove === 'paper' && userMove === 'scissors') || (computerMove === 'scissors' && userMove === 'rock')) {
         result = 'YOU WIN';
         scores.wins += 1;
     }
 
     localStorage.setItem("scores", JSON.stringify(scores));
 
-    console.log(displayScores(userMove, compMove, scores.wins, scores.losses, scores.ties, result));
-    return displayScores(userMove, compMove, scores.wins, scores.losses, scores.ties, result);
+    console.log(displayScores(userMove, computerMove, scores.wins, scores.losses, scores.ties, result));
+    return displayScores(userMove, computerMove, scores.wins, scores.losses, scores.ties, result);
 
 
 
 }
 
-function displayScores(userMove, compMove, wins, losses, ties, result) {
+function displayScores(userMove, computerMove, wins, losses, ties, result) {
     return gameResult.innerHTML = `<p style="margin-top:50px;margin-bottom:30px;font-weight:bold;font-size:35px;">"${result}"</p>
-            <p style="font-weight:bold">You picked - <img src="images/${userMove}-emoji.png" style="height:60px">, Computer picked - <img src="images/${compMove}-emoji.png" style="height:60px">.</p>
-            <p style="margin-top:50px;font-size:18px;font-weight:bold">Wins: ${wins}, Losses: ${losses}, Ties: ${ties}</p>`;
+            <p style="font-weight:bold">
+                You picked - <img src="images/${userMove}-emoji.png" style="height:60px">, Computer picked - <img src="images/${computerMove}-emoji.png" style="height:60px">.
+            </p>
+            <p style="margin-top:50px;font-size:18px;font-weight:bold">
+                Wins: ${wins}, Losses: ${losses}, Ties: ${ties}
+            </p>`;
 
 }
 
